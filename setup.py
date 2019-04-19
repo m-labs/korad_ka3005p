e0adec6c577d7d1b832a2b1bf20e89ac393ca27e
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-04-19 19:45:52 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="korad_ka3005p",
+    install_requires=["asyncserial"],
+    packages=find_packages()
+)
---
+++ test/test_korad_ka3005p.py
@@ -0,0 +1,31 @@
+import sys
+import unittest
+
+from artiq.test.hardware_testbench import GenericControllerCase, ControllerCase
+
+
+class GenericKoradKA3005PTest:
+    def test_parameters_readback(self):
+
+        # check device ID baked into firmware
+        ids = self.driver.get_id()
+        self.assertEqual(ids, "KORADKA3005PV2.0")
+
+
+class TestKoradKA3005P(GenericKoradKA3005PTest, GenericControllerCase):
+    def get_device_db(self):
+        return {
+            "korad_ka3005p": {
+                "type": "controller",
+                "host": "::1",
+                "port": 3256,
+                "command": (sys.executable.replace("\\", "\\\\")
+                            + " -m korad_ka3005p.aqctl_korad_ka3005p "
+                            + "-p {port} --simulation")
+            }
+        }
+
+    def setUp(self):
+        GenericControllerCase.setUp(self)
+        self.start_controller("korad_ka3005p")
+        self.driver = self.device_mgr.get("korad_ka3005p")
