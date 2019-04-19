0f36670b2231db9abdbc26d253d4d95ed2ae353d
Sébastien Bourdeauducq <sb@m-labs.hk>
2019-04-19 23:07:42 +0800

    setup.py: add entry point

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="korad_ka3005p",
     install_requires=["asyncserial"],
-    packages=find_packages()
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "aqctl_korad_ka3005p = korad_ka3005p.aqctl_korad_ka3005p:main",
+        ],
+    },
 )
